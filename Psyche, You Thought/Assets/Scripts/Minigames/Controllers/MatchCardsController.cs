aa0a2596843522b6805bbafe50fbd514a294bb91
Isabella Nguyen <ipnguye1@uci.edu>
2026-05-20 17:44:39 -0700

    Finished everything up to making multiple matches
    I'm using this tutorial: https://www.youtube.com/playlist?list=PLZhNP5qJ2IA2DA4bzDyxFMs8yogVQSrjW
    
    I need to implement logic to enable making multiple matches as well as a life/points system but I made progress

--- Psyche, You Thought/Assets/Scripts/Minigames/Controllers/MatchCardsController.cs
+++ Psyche, You Thought/Assets/Scripts/Minigames/Controllers/MatchCardsController.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class MatchCardsController : MinigameController
 {
-        private bool  _running;
+    private bool _running;
+
+    [SerializeField]
+    private Sprite cardBack;
+
+    public Sprite[] frontImages;
+    public List<Sprite> cardFronts = new List<Sprite>();
+
+    public List<Button> cards = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+    private string firstGuessCard, secondGuessCard;
+
+    private void Awake() {
+        frontImages = Resources.LoadAll<Sprite>("match_card_fronts");
+    }
 
     public override void StartMinigame()
     {
@@ -12,7 +35,69 @@ public class MatchCardsController : MinigameController
         _running       = true;
 
         Debug.Log("[MatchCardsController] Minigame started.");
+
+        //generate cards?
+        //fill in positions
+
+        //for every card: pick a position. if it's taken, reroll. if not, claim the position
+
         // TODO
+
+        GetCards();
+        AddListeners();
+        AddCardFronts();
+    }
+
+    void GetCards() {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("MatchCard");
+
+        for (int i = 0; i < objects.Length; i++) {
+            cards.Add(objects[i].GetComponent<Button>());
+            cards[i].image.sprite = cardBack;
+        }
+
+    }
+
+    void AddCardFronts() {
+        int index = 0;
+        for (int i = 0; i < cards.Count; i++) {
+            if (index == cards.Count/2) {
+                index = 0;
+            }
+            cardFronts.Add(frontImages[index]);
+            index++;
+        }
+    }
+
+    void AddListeners() {
+        foreach (Button card in cards) {
+            card.onClick.AddListener(() => PickACard());
+        }
+    }
+
+    public void PickACard() {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        
+        if (!firstGuess) {
+            firstGuess = true;
+            firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            firstGuessCard = cardFronts[firstGuessIndex].name;
+            cards[firstGuessIndex].image.sprite = cardFronts[firstGuessIndex];
+        } else if (!secondGuess) {
+            secondGuess = true;
+            secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            secondGuessCard = cardFronts[secondGuessIndex].name;
+            cards[secondGuessIndex].image.sprite = cardFronts[secondGuessIndex];
+
+            if (firstGuessCard == secondGuessCard) {
+                Debug.Log("yay");
+            } else {
+                Debug.Log("nay");
+            }
+
+        }
+
+            Debug.Log("haha jonathan you are clicking " + name);
     }
 
     private void Update()
